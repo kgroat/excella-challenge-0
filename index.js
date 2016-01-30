99ea44481a0c150d593e12a86d2beb5330bfc7a1
Kevin Groat <kgroat09@gmail.com>
2016-01-30 09:40:40 -0500

    anagram

---
+++ index.js
@@ -0,0 +1,46 @@
+/**
+ * Created by kevin on 1/30/16.
+ */
+
+var express = require('express')
+var app = express()
+
+app.post('/anagram', function(req, res){
+    var output = []
+    var i, j, list, first, letters, works
+    for(i=0; i<req.params.length; i++){
+        list = req.params[i]
+        first = list[0]
+        works = true
+        letters = getLetterCounts(first)
+        for(j=1; j<list.length; j++){
+            works &= testLetters(list[i], letters)
+        }
+        output.push(works)
+    }
+    return output
+})
+
+function getLetterCounts(str){
+    var letters = {}
+    for(var i=0; i<str.length; i++){
+        letters[str[i]] = (letters[str[i]] || 0) + 1;
+    }
+    return letters
+}
+
+function testLetters(str, letters){
+    var otherLetters = getLetterCounts(str)
+    var l
+    for(l in letters){
+        if(otherLetters[l] != letters[l]){
+            return false
+        }
+    }
+    for(l in otherLetters){
+        if(otherLetters[l] != letters[l]){
+            return false
+        }
+    }
+    return true
+}
